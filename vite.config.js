b77a0ff28afca96e321bdd753733a13e24666c4d
AlexandreGlatz <aglatz@gaming.tech>
2025-04-09 00:03:02 +0200

    fix router

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/docs/',
     plugins: [vue()],
     build: {
         outDir: './docs', // Ensure this matches the outDir in tsconfig.json
