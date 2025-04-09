50a2da3fa6a489606ca2725888508b9a3b3de120
AlexandreGlatz <aglatz@gaming.tech>
2025-04-09 17:23:14 +0200

    bb

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ const routes = [
     },
 ];
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/alexandreglatz.github.io/'),
     routes,
 });
 export default router;
